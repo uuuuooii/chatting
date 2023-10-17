662250a7c4a338ec2dbc490c6ce395f636c5b61c
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-17 20:30:15 +0900

    Update: 프론트엔드 코드 추가

--- utils/io.js
+++ utils/io.js
@@ -1,1 +1,9 @@
-module.exports = function (io) {};
+module.exports = function (io) {
+  // 말하는 함수 .emit()
+  // 듣는 함수 .on()
+
+  // 연결된 사람의 정보를 매개변수로 보내줌
+  io.on('connection', async (socket) => {
+    console.log('client is connected', socket.id);
+  });
+};

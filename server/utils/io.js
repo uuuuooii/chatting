232bf8a463c8729ac69ecc01abdb14249d42c9e2
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-18 22:25:08 +0900

    Update: 서버에서 login 데이터 받기

--- server/utils/io.js
+++ server/utils/io.js
@@ -6,6 +6,8 @@ module.exports = function (io) {
   io.on('connection', async (socket) => {
     console.log('client is connected', socket.id);
 
+    socket.on('login', (userName) => {});
+
     socket.on('disconnect', () => {
       console.log('user is disconnected');
     });

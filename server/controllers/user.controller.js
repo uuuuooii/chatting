201c26943b6ef31a0318ea5beb35cf33bfb89d25
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-19 20:41:39 +0900

    Update: 서버에서 유저네임 찾기

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,10 @@
+const User = require('../models/user');
 const userController = {};
 
-userController.saveUser = async () => {};
+userController.saveUser = async (userName, sid) => {
+  // 이미 있는 유저인지 확인
+  const user = await User.findOne({ name: userName });
+  // 없다면 새로 유저정보 만들기
+  // 이미 있는 유저라면 연결 정보 token값만 바꿔주자
+};
 module.exports = userController;

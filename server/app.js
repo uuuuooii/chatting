d83408961aaddf5ddbf6e3732e23789406f2bcc5
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-22 16:24:30 +0900

    Fix: 에러 확인

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(cors());
 
